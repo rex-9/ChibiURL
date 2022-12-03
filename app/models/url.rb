8d8be8746bbb2110e142a568465fb053bdd7f0a8
Htet Naing <htetnaing0814@gmail.com>
2022-12-03 21:21:55 +0630

    Add generate short url if short is null or empty

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,4 +4,12 @@ class Url < ApplicationRecord
   validates :clicks, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   validates_length_of :short, within: 3..16, on: :create, message: "minimum 3 characters, maximum 16 characters"
+
+  before_validation :generate_short
+
+  private
+
+  def generate_short
+    self.short = SecureRandom.uuid[0..5] if self.short.nil? || self.short.empty?
+  end
 end
